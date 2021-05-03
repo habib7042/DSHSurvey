5acd2680d253823d16428fc2d85be4dd3a7d51db
MD Ahasan Habib <ahasanh303@gmail.com>
2021-05-03 16:29:12 +0600

    bd gadget

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react'
-import UserForm from './components/FormUserInfo'
+import { Grid } from "@material-ui/core";
+import React from "react";
+import Content from "./components/Content";
+import Header from "./components/Header";
 
 function App() {
-    return (
-        <div>
-            <UserForm/>
-        </div>
-    )
+  return (
+    <Grid container direction="column">
+      <Grid item>
+        <Header/>
+      </Grid>
+      <Grid item container>
+        <Grid item xs={false} sm={2} />
+        <Grid item xs={12} sm={8}>
+          <Content/>
+        </Grid>
+        <Grid item xs={false} sm={2} />
+      </Grid>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
